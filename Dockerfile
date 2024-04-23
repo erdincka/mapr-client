19712b444e07a5f3d1ed9aa707c92bc7d3b48458
Erdinc Kaya <erdincka@msn.com>
2024-04-23 19:37:32 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+FROM --platform=linux/amd64 ubuntu:20.04
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt update
+RUN apt upgrade -y
+RUN apt install -y ca-certificates locales locales syslinux syslinux-utils
+ENV SHELL=/bin/bash \
+    LC_ALL=en_US.UTF-8 \
+    LANG=en_US.UTF-8 \
+    LANGUAGE=en_US.UTF-8
+RUN locale-gen $LC_ALL
+RUN apt install -y --no-install-recommends gnupg2 python3-pip python3-dev wget curl \
+    libgcc1 openjdk-11-jdk openssh-client nfs-common build-essential lsb-core libcurl3-gnutls
+
+## Ensure user
+ENV MAPR_USER=mapr
+ENV MAPR_PASS=mapr
+RUN useradd -u 5000 -U -m -d /home/${MAPR_USER} -s /bin/bash -G sudo ${MAPR_USER}
+RUN echo "${MAPR_USER}:${MAPR_PASS}" | chpasswd
+
+## Enable mapr repository
+COPY ./mapr.list /etc/apt/sources.list.d/mapr.list
+# COPY ./auth.conf /etc/apt/auth.conf.d/mapr.conf
+COPY ./maprgpg.key /tmp/maprgpg.key
+RUN cat /tmp/maprgpg.key | apt-key add -
+# RUN echo /tmp/maprgpg.key | gpg --dearmor -o /usr/share/keyrings/mapr-archive.gpg
+RUN apt clean
+RUN apt update
+# Install the mapr packages
+RUN apt install -y mapr-core mapr-client mapr-posix-client-basic
+
+# Enable streams and db clients
+RUN pip3 install --global-option=build_ext --global-option="--library-dirs=/opt/mapr/lib" --global-option="--include-dirs=/opt/mapr/include/" mapr-streams-python
+RUN pip3 install maprdb-python-client
+ENV LD_LIBRARY_PATH=/opt/mapr/lib
+ENV JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+# ## Enable spark and delta lake
+RUN pip3 install pandas numpy wheel faker pyspark tqdm boto3
+RUN apt install -y mapr-spark
+RUN pip3 install requests delta-spark==2.3.0 avro-python3==1.10.2
+# RUN pip3 install jupyterlab
+RUN apt install -y putty
+
+ADD ./start.sh /start.sh
+RUN chmod +x /start.sh
+
+ADD ./app /workspace
+# Finalize client configuration
+WORKDIR /workspace
+EXPOSE 8080
+EXPOSE 8090
+EXPOSE 22
+ENTRYPOINT [ "/start.sh" ]
---
+++ start.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "${MAPR_IP} ${MAPR_CLUSTER}" | tee -a /etc/hosts
+
+# curl --insecure --user ${MAPR_USER}:${MAPR_PASS} -T /opt/mapr/conf/ sftp://${MAPR_IP}/opt/mapr/conf/ssl_truststore
+echo n | pscp -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore /opt/mapr/conf/
+echo n | pscp -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore.p12 /opt/mapr/conf/
+echo n | pscp -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore.pem /opt/mapr/conf/
+echo n | pscp -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/maprtrustcreds.conf /opt/mapr/conf/
+echo n | pscp -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/maprtrustcreds.jceks /opt/mapr/conf/
+echo n | pscp -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_keystore-signed.pem /opt/mapr/conf/
+# scp -o 'StrictHostKeyChecking no' ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/maprtrustcreds.conf /opt/mapr/conf/
+# scp -o 'StrictHostKeyChecking no' ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/maprtrustcreds.jceks /opt/mapr/conf/
+# scp -o 'StrictHostKeyChecking no' ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore /opt/mapr/conf/
+# scp -o 'StrictHostKeyChecking no' ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore.pem /opt/mapr/conf/
+# scp -o 'StrictHostKeyChecking no' ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore.p12 /opt/mapr/conf/
+# scp -o 'StrictHostKeyChecking no' ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_keystore-signed.pem /opt/mapr/conf/
+
+/opt/mapr/server/configure.sh -N "${MAPR_CLUSTER:-maprdemo.mapr.io}" -c -C "${MAPR_IP}":7222 -HS "${MAPR_IP}"
+echo "Client configured"
+
+echo -n "Logging into ${MAPR_CLUSTER}"
+while [ ! -f /tmp/maprticket_0 ] ; do
+  sleep 10
+  echo -n .
+  echo "${MAPR_PASS:-mapr}" | maprlogin password -user "${MAPR_USER:-mapr}" -cluster "${MAPR_CLUSTER:-maprdemo.mapr.io}"
+done
+
+maprlogin generateticket -user "${MAPR_USER:-mapr}" -type service -out /opt/mapr/conf/maprfuseticket -duration 30:0:0 -renewal 90:0:0
+[ -d /mapr ] || mkdir /mapr
+service mapr-posix-client-basic start
+echo "posix client configured"
+
+
+cd /workspace
+# jupyter lab --allow-root --ip=0.0.0.0 --port=8887 --no-browser
+echo "Sleep forever..."
+while :; do :; done & kill -STOP $! && wait $!
+# java -cp $(mapr classpath):/app/ezshow-1.0-jar-with-dependencies.jar lab.kaya.Ezshow
