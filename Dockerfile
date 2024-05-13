e5f5ef798ff19d26e6617ecd9e4507d9404bf30a
Erdinc Kaya <erdincka@msn.com>
2024-05-13 01:17:26 +0100

    streamline

--- Dockerfile
+++ Dockerfile
@@ -13,27 +13,18 @@ ENV SHELL=/bin/bash \
     LANGUAGE=en_US.UTF-8
 RUN locale-gen $LC_ALL
 RUN apt install -y --no-install-recommends gnupg2 python3-pip python3-dev wget curl \
-    libgcc1 openjdk-11-jdk openssh-client openssh-server nfs-common build-essential lsb-core libcurl3-gnutls putty sudo
-
-## Ensure user
-ENV MAPR_USER=mapr
-ENV MAPR_PASS=mapr
-RUN useradd -u 5000 -U -m -d /home/${MAPR_USER} -s /bin/bash -G sudo ${MAPR_USER}
-RUN echo "${MAPR_USER}:${MAPR_PASS}" | chpasswd
-RUN echo "root:${MAPR_PASS}" | chpasswd
-RUN echo "mapr ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/mapr
+    libgcc1 openjdk-11-jdk openssh-client openssh-server nfs-common build-essential \
+    lsb-core libcurl3-gnutls putty sudo git
 
 ## Enable mapr repository
 COPY ./mapr.list /etc/apt/sources.list.d/mapr.list
-# COPY ./auth.conf /etc/apt/auth.conf.d/mapr.conf
 COPY ./maprgpg.key /tmp/maprgpg.key
 RUN cat /tmp/maprgpg.key | apt-key add -
-# RUN echo /tmp/maprgpg.key | gpg --dearmor -o /usr/share/keyrings/mapr-archive.gpg
 RUN apt clean
 RUN apt update
 
 # Install the mapr packages
-RUN apt install -y mapr-core mapr-client mapr-posix-client-basic
+RUN apt install -y mapr-core mapr-client mapr-posix-client-basic mapr-spark
 
 # Enable streams and db clients
 RUN pip3 install --global-option=build_ext --global-option="--library-dirs=/opt/mapr/lib" --global-option="--include-dirs=/opt/mapr/include/" mapr-streams-python
@@ -41,10 +32,10 @@ RUN pip3 install maprdb-python-client
 ENV LD_LIBRARY_PATH=/opt/mapr/lib
 ENV JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 
-RUN apt install -y mapr-spark git
+EXPOSE 22
+EXPOSE 3000
 
 ADD ./start.sh /start.sh
 RUN chmod +x /start.sh
 
-EXPOSE 22
 ENTRYPOINT [ "/start.sh" ]
--- start.sh
+++ start.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 
-echo n | pscp -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore /opt/mapr/conf/
+set -euo pipefail
+
+## Ensure user
+useradd -u 5000 -U -m -d /home/${MAPR_USER} -s /bin/bash -G sudo ${MAPR_USER}
+echo "${MAPR_USER}:${MAPR_PASS}" | chpasswd
+echo "root:${MAPR_PASS}" | chpasswd
+echo "mapr ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/mapr
+
+echo n | pscp -P 22 -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore /opt/mapr/conf/
 echo n | pscp -P 22 -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore.p12 /opt/mapr/conf/
 echo n | pscp -P 22 -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_truststore.pem /opt/mapr/conf/
 echo n | pscp -P 22 -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/maprtrustcreds.conf /opt/mapr/conf/
 echo n | pscp -P 22 -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/maprtrustcreds.jceks /opt/mapr/conf/
 echo n | pscp -P 22 -pw "${MAPR_PASS}" -r ${MAPR_USER}@${MAPR_IP}:/opt/mapr/conf/ssl_keystore-signed.pem /opt/mapr/conf/
 
-/opt/mapr/server/configure.sh -N "${MAPR_CLUSTER:-maprdemo.mapr.io}" -c -C "${MAPR_IP}":7222 -HS "${MAPR_IP}"
+/opt/mapr/server/configure.sh -N "${MAPR_CLUSTER}" -c -C "${MAPR_IP}":7222 -HS "${MAPR_IP}"
 echo "Client configured"
 
-echo -n "Logging into ${MAPR_CLUSTER}"
+echo -n "Connecting to ${MAPR_CLUSTER}"
 while [ ! -f /tmp/maprticket_0 ] ; do
   sleep 5
   echo -n .
-  echo "${MAPR_PASS:-mapr}" | maprlogin password -user "${MAPR_USER:-mapr}" -cluster "${MAPR_CLUSTER:-maprdemo.mapr.io}"
+  echo "${MAPR_PASS}" | maprlogin password -user "${MAPR_USER}" -cluster "${MAPR_CLUSTER}"
 done
-
-# Enable posix client
-# maprlogin generateticket -user "${MAPR_USER:-mapr}" -type service -out /opt/mapr/conf/maprfuseticket -duration 30:0:0 -renewal 90:0:0
-# [ -d /mapr ] || mkdir /mapr
-# service mapr-posix-client-basic start
-# echo "posix client configured"
 
 # enable ssh
 service ssh start
 
-echo "Sleep forever..."
-while :; do :; done & kill -STOP $! && wait $!
+# echo "Sleep forever..."
+# while :; do :; done & kill -STOP $! && wait $!
+
+exec "$@"
